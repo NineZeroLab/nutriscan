ca812bb9c453aae2f2c01f53b32dbee08234e661
kamalnathvg <kamalnath7171@gmail.com>
2024-06-29 20:37:33 -0400

    increased timeout limit for retrofit client

--- app/src/main/java/com/zero1labs/nutriscan/di/AppModule.kt
+++ app/src/main/java/com/zero1labs/nutriscan/di/AppModule.kt
@@ -8,8 +8,10 @@ import com.zero1labs.nutriscan.repository.AppRepository
 import com.zero1labs.nutriscan.utils.AppResources.BASE_URL
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -20,10 +22,21 @@ object AppModule{
     @Singleton
     fun providesAppApi() : Retrofit{
         val gson = GsonBuilder().setLenient().create()
+        val connectTimeout : Long = 30
+        val readTimeout : Long = 30
+        val writeTimeout : Long = 30
+
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(connectTimeout,TimeUnit.SECONDS)
+            .readTimeout(readTimeout, TimeUnit.SECONDS)
+            .writeTimeout(writeTimeout, TimeUnit.SECONDS)
+            .build()
+
         val retrofit : Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create(gson))
-        .build()
+             .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
 
         return retrofit
     }
