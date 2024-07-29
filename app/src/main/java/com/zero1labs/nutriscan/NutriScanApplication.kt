59e80d5e039bbaca3c7ada71e8da8659b48fc3ab
kamalnathvg <kamalnath7171@gmail.com>
2024-07-29 15:50:16 -0400

    -made some changes

--- app/src/main/java/com/zero1labs/nutriscan/NutriScanApplication.kt
+++ app/src/main/java/com/zero1labs/nutriscan/NutriScanApplication.kt
@@ -4,4 +4,8 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class NutriScanApplication : Application()
+class NutriScanApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
