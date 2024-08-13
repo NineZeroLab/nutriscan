88b4e74b9e4ade2e41eb1cb931d775e897151c5e
kamalnathvg <kamalnath7171@gmail.com>
2024-08-13 19:15:37 -0400

    added refactored code using extension functions

---
+++ app/src/main/java/com/zero1labs/nutriscan/utils/AndroidExtensions.kt
@@ -0,0 +1,38 @@
+package com.zero1labs.nutriscan.utils
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import kotlin.time.Duration
+
+fun View.hide(){
+    this.visibility = View.GONE
+}
+
+fun View.show(){
+    this.visibility = View.VISIBLE
+}
+
+fun View.showSnackBar(message: String, duration: Int = Snackbar.LENGTH_LONG){
+    Snackbar.make(this, message, duration).show()
+}
+
+fun View.hideKeyBoard(){
+    val imm: InputMethodManager = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(this.windowToken, 0)
+}
+
+fun View.showKeyboard(){
+    val imm: InputMethodManager = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(this, 0)
+}
+fun TextInputLayout.removeError(){
+    this.error = null
+}
+
+fun TextInputLayout.getInput(): String{
+    return  this.editText?.text.toString()
+}
+
---
+++ app/src/main/java/com/zero1labs/nutriscan/utils/KotlinExtensions.kt
@@ -0,0 +1,20 @@
+package com.zero1labs.nutriscan.utils
+
+import android.util.Log
+import com.zero1labs.nutriscan.utils.AppResources.TAG
+
+fun String.isValidEmail(): Pair<Boolean, String>{
+//    TODO()
+    return Pair(true,"")
+}
+fun String.isValidPassword(): Pair<Boolean, String>{
+//    TODO()
+    return Pair(true,"")
+}
+fun logger(message: String){
+    Log.d(TAG, message)
+}
+
+
+
+
