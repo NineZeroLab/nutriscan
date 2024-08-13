e944fe04794061e9eb2d2cdd5d46056778510806
kamalnathvg <kamalnath7171@gmail.com>
2024-08-13 19:34:06 -0400

    refactored homepage

--- app/src/main/java/com/zero1labs/nutriscan/pages/homepage/HomePage.kt
+++ app/src/main/java/com/zero1labs/nutriscan/pages/homepage/HomePage.kt
@@ -29,59 +29,163 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 import com.zero1labs.nutriscan.R
 import com.zero1labs.nutriscan.data.models.SearchHistoryListItem
+import com.zero1labs.nutriscan.databinding.FragmentHomePageBinding
 import com.zero1labs.nutriscan.ocr.BarCodeScannerOptions
 import com.zero1labs.nutriscan.utils.AppResources
 import com.zero1labs.nutriscan.utils.AppResources.TAG
+import com.zero1labs.nutriscan.utils.hide
+import com.zero1labs.nutriscan.utils.logger
+import com.zero1labs.nutriscan.utils.show
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
-class HomePage : Fragment(R.layout.fragment_home_page) {
+class HomePage : Fragment() {
 
-    private lateinit var progressBar: ProgressBar
-    private lateinit var progressBarBg : View
-    private lateinit var rvSearchHistoryItems : RecyclerView
-    private lateinit var fabScanProduct : FloatingActionButton
-    private lateinit var fabGetDemoItem: FloatingActionButton
-    private lateinit var tvUserName: TextView
-    private lateinit var materialToolbar: MaterialToolbar
     private lateinit var viewModel: HomePageViewModel
+    private lateinit var viewBinding: FragmentHomePageBinding
+    private lateinit var materialToolbar: MaterialToolbar
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = ViewModelProvider(requireActivity())[HomePageViewModel::class.java]
+        viewBinding = FragmentHomePageBinding.inflate(inflater, container, false)
+        return  viewBinding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(requireActivity())[HomePageViewModel::class.java]
-        progressBar = view.findViewById(R.id.progress_bar)
-        progressBarBg = view.findViewById(R.id.blurBg)
-        rvSearchHistoryItems = view.findViewById(R.id.rv_search_history)
-        fabScanProduct = view.findViewById(R.id.fab_scan_product)
-        fabGetDemoItem = view.findViewById(R.id.fab_get_demo_item)
+        buildToolbar()
+        viewModel.onEvent(HomePageEvent.UpdateUserDetails)
+
+
+        Log.d(TAG,"Products from firebase: ${viewModel.uiState.value.searchHistory}")
+
+        handleScanButton()
+
+        handleDemoItemButton()
+        buildSearchHistoryRv()
+        handleUiState(view)
+    }
+
+    private fun handleUiState(view: View) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.uiState.flowWithLifecycle(
+                viewLifecycleOwner.lifecycle,
+                Lifecycle.State.RESUMED
+            )
+                .collectLatest { state ->
+                    when (state.productScanState) {
+                        ProductScanState.Success -> {
+                            hideProgressBar()
+                            logger(state.productScanState.name)
+                            findNavController().navigate(R.id.action_home_page_to_product_details_page)
+                        }
+
+                        ProductScanState.Failure -> {
+                            hideProgressBar()
+                            logger(state.productScanState.name)
+                            Snackbar.make(view, state.msg.toString(), Snackbar.LENGTH_LONG).show()
+    //                        findNavController().navigate(R.id.action_home_page_to_error_page)
+                        }
+
+                        ProductScanState.Loading -> {
+                            logger(state.productScanState.name)
+                            showProgressBar()
+                        }
+
+                        ProductScanState.NotStarted -> {
+                            logger(state.productScanState.name)
+                        }
+                    }
+
+                    when (state.firebaseDataFetchState) {
+                        FirebaseDataFetchState.Loading -> {
+                            showProgressBar()
+                            logger(state.firebaseDataFetchState.name)
+                        }
+                        FirebaseDataFetchState.Success -> {
+                            hideProgressBar()
+                            materialToolbar.title = "Hi ${state.appUser?.name}"
+                            logger(state.firebaseDataFetchState.name)
+                            (viewBinding.rvSearchHistory.adapter as SearchHistoryAdapter).updateData(
+                                state.searchHistory
+                            )
+                        }
+
+                        FirebaseDataFetchState.Failure -> {
+                            hideProgressBar()
+                            logger(state.firebaseDataFetchState.name)
+                        }
+
+                        FirebaseDataFetchState.NotStarted -> {
+                            logger(state.firebaseDataFetchState.name)
+                            if (state.appUser == null) {
+                                logger("app user is null... navigating to sign in page")
+                                findNavController().navigate(R.id.action_home_page_to_sign_in_page)
+                            }
+                        }
+                    }
+                }
+        }
+    }
+
+    private fun buildSearchHistoryRv() {
+        val searchHistoryItems : List<SearchHistoryListItem> = viewModel.uiState.value.searchHistory
+        val searchHistoryAdapter = SearchHistoryAdapter(viewModel,searchHistoryItems)
+        viewBinding.rvSearchHistory.layoutManager = LinearLayoutManager(requireContext())
+        viewBinding.rvSearchHistory.adapter = searchHistoryAdapter
+    }
+
+    private fun handleDemoItemButton() {
+        viewBinding.fabGetDemoItem.setOnClickListener {
+            Log.d("logger", "Get Demo Item fab clicked")
+            viewModel.onEvent(HomePageEvent.FetchProductDetails(AppResources.getRandomItem()))
+
+        }
+    }
+
+    private fun handleScanButton() {
+        viewBinding.fabScanProduct.setOnClickListener() {
+            Log.d("logger", "starting gms barcode scanner")
+            val scanner =
+                GmsBarcodeScanning.getClient(requireContext(), BarCodeScannerOptions.options)
+            scanner.startScan()
+                .addOnSuccessListener { barcode ->
+                    logger("barcode scan success")
+                    viewModel.onEvent(HomePageEvent.FetchProductDetails(barcode.rawValue.toString()))
+                }
+                .addOnCanceledListener {
+                    logger("barcode scan cancelled")
+                }
+                .addOnFailureListener {
+                    logger("barcode scan failure")
+                }
+        }
+    }
+
+    private fun buildToolbar() {
         val appCompatActivity: AppCompatActivity = activity as AppCompatActivity
         materialToolbar = appCompatActivity.findViewById(R.id.mt_app_toolbar)
         appCompatActivity.setSupportActionBar(materialToolbar)
-        viewModel.onEvent(HomePageEvent.UpdateUserDetails)
-
         viewModel.uiState.value.appUser.let { user ->
             if (user != null){
                 materialToolbar.title = "Hi ${user.name}"
                 materialToolbar.overflowIcon?.setTint(ContextCompat.getColor(requireContext(),R.color.md_theme_onPrimary))
             }
         }
-        Log.d(TAG,"Products from firebase: ${viewModel.uiState.value.searchHistory}")
 
         requireActivity().addMenuProvider(object: MenuProvider{
             override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
                 menuInflater.inflate(R.menu.homepage_menu,menu)
-                val appUser = viewModel.uiState.value.appUser
-                menu.findItem(R.id.mi_sign_out)?.isVisible = appUser != null
-                menu.findItem(R.id.mi_sign_in)?.isVisible = appUser == null
             }
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
                 return when(menuItem.itemId){
                     R.id.mi_sign_out -> {
                         viewModel.onEvent(HomePageEvent.SignOut)
-                        true
-                    }
-                    R.id.mi_sign_in -> {
-                        findNavController().navigate(R.id.action_home_page_to_sign_in_page)
                         true
                     }
                     R.id.mi_edit_profile -> {
@@ -93,98 +197,24 @@ class HomePage : Fragment(R.layout.fragment_home_page) {
             }
         },viewLifecycleOwner)
 
-        fabScanProduct.setOnClickListener(){
-
-            Log.d("logger", "starting gms barcode scanner")
-
-            val scanner = GmsBarcodeScanning.getClient(requireContext(), BarCodeScannerOptions.options)
-            scanner.startScan()
-            .addOnSuccessListener { barcode ->
-                Log.d("logger", "product scan successfully")
-                viewModel.onEvent(HomePageEvent.FetchProductDetails(barcode.rawValue.toString()))
-            }
-            .addOnCanceledListener {
-                Log.d("logger", "action cancelled by user")
-            }
-            .addOnFailureListener {
-                Log.d("logger", it.message.toString())
-            }
-        }
-
-        fabGetDemoItem.setOnClickListener{
-            Log.d("logger", "Get Demo Item fab clicked")
-            viewModel.onEvent(HomePageEvent.FetchProductDetails(AppResources.getRandomItem()))
-
-        }
-        val searchHistoryItems : List<SearchHistoryListItem> = viewModel.uiState.value.searchHistory
-        val searchHistoryAdapter = SearchHistoryAdapter(viewModel,searchHistoryItems)
-        rvSearchHistoryItems.layoutManager = LinearLayoutManager(requireContext())
-        rvSearchHistoryItems.adapter = searchHistoryAdapter
-
-
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewModel.uiState.flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.RESUMED)
-                .collectLatest {state ->
-                    when(state.productScanState){
-                        ProductScanState.Success -> {
-                            hideProgressBar()
-                            Log.d("logger","Product fetch success")
-                            findNavController().navigate(R.id.action_home_page_to_product_details_page)
-                        }
-                        ProductScanState.Failure ->{
-                            hideProgressBar()
-                            Log.d("logger","Product fetch failure")
-                            Snackbar.make(view,state.msg.toString(),Snackbar.LENGTH_LONG).show()
-//                        findNavController().navigate(R.id.action_home_page_to_error_page)
-                        }
-                        ProductScanState.Loading -> {
-
-                            Log.d("logger","Loading Product Details...")
-                            showProgressBar()
-                        }
-                        ProductScanState.NotStarted -> Log.d("logger" , "product Scan not started")
-                    }
-
-                    when(state.firebaseDataFetchState){
-                        FirebaseDataFetchState.Loading -> {
-                            Log.d("logger","Firebase Data Fetch State : Loading")
-                            showProgressBar()
-                        }
-                        FirebaseDataFetchState.Success -> {
-                            hideProgressBar()
-                            materialToolbar.title = "Hi ${state.appUser?.name}"
-                            Log.d("logger","Firebase Data Fetch State : Success")
-                            searchHistoryAdapter.updateData(state.searchHistory)
-                        }
-                        FirebaseDataFetchState.Failure -> {
-                            hideProgressBar()
-                            Log.d("logger","Firebase Data Fetch State : Failure")
-                        }
-                        FirebaseDataFetchState.NotStarted -> {
-                            Log.d("logger","Firebase Data Fetch State : Not Started")
-                            if(state.appUser == null){
-                                Log.d("logger","appUser is null")
-                                findNavController().navigate(R.id.action_home_page_to_sign_in_page)
-                            }
-                        }
-                    }
-                }
-        }
     }
 
     private fun showProgressBar(){
-        progressBar.visibility = View.VISIBLE
-        progressBarBg.visibility = View.VISIBLE
-        fabScanProduct.isClickable = false
-        fabGetDemoItem.isClickable = false
-
+        viewBinding.apply {
+            progressBar.show()
+            blurBg.show()
+            fabScanProduct.isClickable = false
+            fabGetDemoItem.isClickable = false
+        }
     }
 
     private fun hideProgressBar(){
-        progressBar.visibility = View.GONE
-        progressBarBg.visibility = View.GONE
-        fabScanProduct.isClickable = true
-        fabGetDemoItem.isClickable = true
+        viewBinding.apply {
+            progressBar.hide()
+            blurBg.hide()
+            fabScanProduct.isClickable = true
+            fabGetDemoItem.isClickable = true
+        }
     }
 
 
